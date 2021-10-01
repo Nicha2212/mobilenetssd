66d82c93025a8bcf72b1189f6d002f3d10cd409c
Nicha2212 <89177932+Nicha2212@users.noreply.github.com>
2021-10-02 00:30:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ UPLOAD_FOLDER ='static/uploads/'
 DOWNLOAD_FOLDER = 'static/downloads/'
 ALLOWED_EXTENSIONS = {'jpg', 'png','.jpeg'}
 
-lineaccesstoken = ''
+lineaccesstoken = 'vQda1c04wNaBDi2M4rulRloDsno/B19eadoZ7Tir3SutvVgBRj1bYzo6pYqC/44ZEX0o6bI+wk6PoNYVn1ydi4LYgbF6xZ7o3/nfgfhxg5rnWzI+V9vQrEbe0OY5UK3KnXw5Atvo7IPQGWYZNC5YIAdB04t89/1O/w1cDnyilFU='
 
 line_bot_api = LineBotApi(lineaccesstoken)
 
